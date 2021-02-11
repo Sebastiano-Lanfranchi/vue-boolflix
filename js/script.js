67d22afd75eab17af6eb3837aa82975b00d13e0d
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2021-02-11 16:06:27 +0100

    fix: vote section

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ new Vue({
     searchFunction: function(){
       this.arraySearches = [];
       this.arraySearches.lenght = 20;
-      axios.get('https://api.themoviedb.org/3/search/multi?api_key=e7e03c0fba8d3369b4b91d7184fa6c83&page=1&include_adult=false&query=' + this.searchMovie)
+      axios.get('https://api.themoviedb.org/3/search/multi?api_key=e7e03c0fba8d3369b4b91d7184fa6c83&language=it&page=1&include_adult=false&query=' + this.searchMovie)
       .then(response => {
         this.arraySearches = response.data.results;
       });
