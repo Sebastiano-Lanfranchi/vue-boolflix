32cfca0817a277318ee9eb75c035dd487e8829ae
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2021-02-11 03:21:46 +0100

    fix: tv and person info

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ new Vue({
   methods:{
     searchFunction: function(){
       this.arraySearches = [];
+      this.arraySearches.lenght = 20;
       axios.get('https://api.themoviedb.org/3/search/multi?api_key=e7e03c0fba8d3369b4b91d7184fa6c83&page=1&include_adult=false&query=' + this.searchMovie)
       .then(response => {
         this.arraySearches = response.data.results;
